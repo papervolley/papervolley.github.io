342918bd11b0a2c864337589b158b51cb167d849
Paper <paper@volleythat.com>
2022-03-23 14:36:34 -0700

    Install tailwind css and routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+import FrontChannel from './FrontChannel';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/frontchannel' element={<FrontChannel />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/FrontChannel.js
@@ -0,0 +1,13 @@
+import './FrontChannel.css';
+import React from "react";
+import ReactDOM from "react-dom";
+
+function FrontChannel() {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold underline'>FrontChannel</h1>
+        </div>
+    );
+}
+
+export default FrontChannel;
