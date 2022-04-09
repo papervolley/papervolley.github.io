3ef189b83a92a9c2dc4fcd5fe3c96dc6a2f72859
Paper <paper@volleythat.com>
2022-04-08 22:31:57 -0700

    Random pick emojis

--- src/FrontChannel.js
+++ src/FrontChannel.js
@@ -7,6 +7,10 @@ import { randomPick, mouseXY } from "./utils.js";
 
 const NOT_NEW_USER_STRING = "noone";
 
+const emojis = [
+    "👍", "❤️", "🎉", "🇺🇦"
+]
+
 const chatlogs  = [
     "hi",
     "renee ding ding you got it right",
@@ -102,24 +106,29 @@ function Prompt(props) {
 
 function InteractionLayer(props) {
     const [scale, setScale] = useState(1);
-    const [scaleInterval, setScaleInterval] = useState();
     const [pressing, setPressing] = useState(false);
     const [mouse, setMouse] = useState([0, 0]);
-
+    const [start, setStart] = useState(false);
+    const [oldPressing, setOldPressing] = useState(false);
     const emojiRef = useRef();
+    const [emoji, setEmoji] = useState(randomPick(emojis));
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setScale(pressing ? Math.min(scale + 0.01, 3) : 1);
+            //setScale(scale => start ? 1 : pressing ? Math.min(scale + 0.01, 3) : scale);
+            setScale(scale => pressing ? oldPressing ? Math.min(scale + 0.01, 3) : 1 : scale);
+            setEmoji(emoji => (pressing && !oldPressing) ? randomPick(emojis) : emoji);
+            setOldPressing(pressing);
         }, 10);
         return () => clearInterval(interval);
-    }, [scale, pressing, mouse]);
+    }, [scale, pressing, mouse, start, oldPressing]);
 
     const startScaling = event => {
         console.log("start");
         //console.log(mouseXY(event));
         setMouse(mouseXY(event));
         setPressing(true);
+        setStart(true);
     }
 
     const stopScaling = event => {
@@ -141,10 +150,22 @@ function InteractionLayer(props) {
     return (
         <>
             <div className="interaction-layer" {...bind}>
-                <div className="emoji-wrapper" ref={emojiRef} style={{transform: `scale(${scale})`, left: mouse[0] - 12, top: mouse[1] - 48, visibility: pressing ? "visible" : "hidden"}}>
-                    <HeartEmoji />
+                <div className={`emoji-box${pressing ? "" : " release"}`} style={{left: mouse[0] - 12, top: mouse[1] - 48}}>
+                    <div className={`emoji-wrapper`} ref={emojiRef} style={{transform: `scale(${scale})`}}>
+                        <RandomEmoji emoji={emoji} />
+                    </div>
                 </div>
             </div>
+        </>
+    )
+}
+
+function RandomEmoji(props) {
+    return (
+        <>
+            <span className='h-full w-full text-yellow-500'>
+                <div className="h-10 w-10">{props.emoji}</div>
+            </span>
         </>
     )
 }
@@ -153,9 +174,9 @@ function HeartEmoji(props) {
     return (
         <>
             <span className='h-full w-full text-yellow-500'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-</svg>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+                </svg>
             </span>
         </>
     )
